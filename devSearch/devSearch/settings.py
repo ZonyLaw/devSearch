cfe2d6ff56d717c18980d6d0a8ba0573e18fa8d1
Zonylaw <sunny_law@hotmail.com>
2023-05-23 21:51:34 +0100

    set up gmail but this doesn't work properly.

--- devSearch/devSearch/settings.py
+++ devSearch/devSearch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from config import GMAIL_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sunnylaw18@gmail.com'
+EMAIL_HOST_PASSWORD = GMAIL_PASSWORD
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
